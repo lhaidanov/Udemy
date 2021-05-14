06dd1630e7a8e4055eac86f4ff314d4bfefa2e82
LH <lhaidanov@determine.com>
2021-05-14 22:09:08 +0300

    add comment to  Homework.Lesson7

--- src/Homework/Lesson7_1/CallEmployee.java
+++ src/Homework/Lesson7_1/CallEmployee.java
@@ -10,9 +10,9 @@ public class CallEmployee {
 
     public static void main(String[] args) {
         Homework.Lesson7.Employee e2 = new Homework.Lesson7.Employee( "Petrov", 18,  "sales");
-//        System.out.println("ID = " + e2.id);
+//        System.out.println("ID = " + e2.id); // id with default access level
         System.out.println("ID = " + e2.surname);
-//        System.out.println("ID = " + e2.salary;
+//        System.out.println("ID = " + e2.salary; // salary with private access level
         e2.ShowID();
         e2.ShowSurname();
         e2.ShowSalary();
