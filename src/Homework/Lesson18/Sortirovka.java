91e405069735beb953ecf2696fcf86678cac77b9
lhaidanov <37084446+lhaidanov@users.noreply.github.com>
2021-06-09 12:08:38 +0300

    lesson18.sortirovka

---
+++ src/Homework/Lesson18/Sortirovka.java
@@ -0,0 +1,35 @@
+package Homework.Lesson18;
+
+public class Sortirovka {
+    public static int[] sortirovka(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            int a;
+            int minElement = array[i];
+            int index = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < minElement) {
+                    minElement = array[j];
+                    index = j;
+                }
+            }
+
+            if (i != index) {
+                a = array[i];
+                array[i] = minElement;
+                array[index] = a;
+            }
+
+        }
+        return array;
+
+    }
+
+    public static void main(String[] args) {
+        int[] array1 = {1, 0, 9, 2};
+        sortirovka(array1);
+        for (int i = 0; i < array1.length; i++) {
+            System.out.print(array1[i] + " ");
+        }
+    }
+}
